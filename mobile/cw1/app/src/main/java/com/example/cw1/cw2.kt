5e7887a748abe8be7a34a65ed19ac077dea521b7
pszczesz <pszczesz@gmail.com>
2023-10-28 10:08:47 +0200

    gg

--- mobile/cw1/app/src/main/java/com/example/cw1/cw2.kt
+++ mobile/cw1/app/src/main/java/com/example/cw1/cw2.kt
@@ -1,10 +1,55 @@
 package com.example.cw1
 
+
+import java.util.Scanner
+import kotlin.random.Random
+
+
 fun main(){
-    for(i in 0..10){
-        println("i = $i")
+   // cw1()
+    cw2()
+}
+fun cw2(){
+    println(isPrimal(55))
+    print("Podaj zakres liczb: ")
+    val scaner = Scanner(System.`in`);
+    var limit = scaner.nextInt()
+    limit = if(limit<0) -limit else limit
+    GenerPrimal(limit)
+}
+fun GenerPrimal(limit:Int){
+    if(limit<2) println("Brak liczb pierwszych w przedziale (0,$limit)")
+    for (i in 2..limit){
+        if(isPrimal(i)) print("$i ")
+    }
+    println("wszystkie liczby pierwsze w przedziale (0,$limit)")
+}
+fun isPrimal(number:Int):Boolean {
+    if(number<=1) return false;
+    var i = 2
+    while (i*i<=number){
+        if(number%i==0) return false;
+        i++
+    }
+    return true;
+}
+
+
+fun cw1(){
+    val rnd = Random;
+//    for(i in 0..10){
+//        println("i = $i")
+//    }
+    var number = 12;
+    while(number!=0){
+        number = rnd.nextInt(0,20)
+        print(number.toString()+" ")
     }
 }
 fun test(){
     val a = "test";
+    //a = "dddd";
+    var b = "ggg";
+    b = "fffff";
+
 }
