dfce0377f0977a277124f679650e9579b239e940
pszczesz <pszczesz@gmail.com>
2023-11-19 14:48:12 +0100

    hh

--- mobile/cw2_view/app/src/main/java/com/example/cw2_view/MainActivity.kt
+++ mobile/cw2_view/app/src/main/java/com/example/cw2_view/MainActivity.kt
@@ -9,17 +9,24 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val obrazek = findViewById<ImageView>(R.id.zyrafa_id)
         val textInfo = findViewById<TextView>(R.id.text_id)
         val btnReset = findViewById<Button>(R.id.btnReset)
-        var counter = 0;
+        val btn2 = findViewById<Button>(R.id.btn2)
+        btn2.setOnClickListener {
+          //  it->{
+                NewFunction()
+            textInfo.text = "żyrafa jest kliknięta $counter razy"
+       // }
+        }
         obrazek.setOnClickListener {
             counter++
-            textInfo.setText("żyrafa jest kliknięta $counter razy")
-            Toast.makeText(this@MainActivity,"Hello from Toast!!",Toast.LENGTH_LONG)
+            textInfo.text = "żyrafa jest kliknięta $counter razy"
+            Toast.makeText(this@MainActivity,"Hello from Toast!!"+btn2.text.toString(),Toast.LENGTH_LONG)
                 .show()
         }
         btnReset.setOnClickListener {
@@ -29,6 +36,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+    public fun NewFunction(){
+        counter--
     }
 
 }
