a0f76e4386fd73dfc2ab37df600aa554a951a776
pszczesz <pszczesz@gmail.com>
2023-11-25 11:05:29 +0100

    nowy

--- mobile/cw3_NWD/app/src/main/java/com/example/cw3_nwd/NWDCounter.kt
+++ mobile/cw3_NWD/app/src/main/java/com/example/cw3_nwd/NWDCounter.kt
@@ -1,22 +1,24 @@
 package com.example.cw3_nwd
 
-class NWDCounter(a:Int=10,b:Int=25) {
-    private var _a:Int
-    private var _b:Int;
-        var A:Int
+class NWDCounter {
+//    private var _a:Int
+//    private var _b:Int;
+//        var A:Int
+//
+//        get() {return _a;}
+//        set(value) {
+//            _a = if (value>0)value else -value
+//        }
+//
+//    var B:Int
+//        get() {return _b}
+//        set(value) {
+//            if (value>0)value else -value}
+//    init {
+//        this._a = a;
+//        this._b = b;
+//    }
+    public fun GetRekNWD(a:Int,b:Int):Int {
 
-        get() {return _a;}
-        set(value) {
-            _a = if (value>0)value else -value
-        }
-
-    var B:Int
-        get() {return _b}
-        set(value) {
-            if (value>0)value else -value}
-    init {
-        this._a = a;
-        this._b = b;
     }
-
 }
