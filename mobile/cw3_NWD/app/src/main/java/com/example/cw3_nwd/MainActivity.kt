3b02a30aa9d5a99c07abc9331041eaa68e6e1218
pszczesz <pszczesz@gmail.com>
2023-11-25 10:37:42 +0100

    gg

---
+++ mobile/cw3_NWD/app/src/main/java/com/example/cw3_nwd/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cw3_nwd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cw3_nwd.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bind:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bind = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(bind.root)
+    }
+}
