06b4bfde1f053175aa49d8cdd77d6d6eb2f4e3e2
pszczesz <pszczesz@gmail.com>
2023-11-26 16:03:51 +0100

    Update MainActivity.kt

--- mobile/cw4_multi_activities/app/src/main/java/com/example/cw4_multi_activities/MainActivity.kt
+++ mobile/cw4_multi_activities/app/src/main/java/com/example/cw4_multi_activities/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.cw4_multi_activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java);
+            startActivity(intent)
+        }
+        btn2.setOnClickListener {
+            val intent=Intent(this,ThirdActivity::class.java)
+            startActivity(intent)
+        }
         Log.d("LIFE","onCreate")
     }
 
