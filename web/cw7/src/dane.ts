a19063b744d1ff4871757135d45f8181b9500290
pszczesz <pszczesz@gmail.com>
2023-11-26 11:24:13 +0100

    gg

---
+++ web/cw7/src/dane.ts
@@ -0,0 +1,11 @@
+export interface Film{
+    title:string;
+    director:string;
+    year:number;
+    price:number
+}
+export const films:Film[]=[
+    {title:"sssss",director:"ggggg",year:2005,price:34.67},
+    {title:"ff",director:"ggggg",year:2005,price:34.67},
+    {title:"www",director:"ggggg",year:2005,price:34.67}
+]
