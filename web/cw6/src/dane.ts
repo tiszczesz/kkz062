7f89c73cf57b148cb1f5179a5bda7a8e44870844
pszczesz <pszczesz@gmail.com>
2023-11-26 09:46:36 +0100

    gg

---
+++ web/cw6/src/dane.ts
@@ -0,0 +1,11 @@
+export interface Student{
+    firstname:string;
+    lastname:string;
+    age:number;
+}
+
+export const students:Student[] = [
+  {firstname:"Antoni",lastname:"Nowak",age:34},
+  {firstname:"Renata",lastname:"Gryn",age:22}
+  
+]
--- web/cw6/src/ex1.ts
+++ web/cw6/src/ex1.ts
@@ -1,2 +1,5 @@
+import { students } from "./dane.js";
 const a:number = 23;
 console.log(`a = ${a}`);
+console.log(students);
+
