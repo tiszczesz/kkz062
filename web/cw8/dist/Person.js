8f4abddd32c73a803b0836e16039de6448db5985
pszczesz <pszczesz@gmail.com>
2023-11-26 13:48:07 +0100

    gg

---
+++ web/cw8/dist/Person.js
@@ -0,0 +1,22 @@
+export class Person {
+    firstname;
+    lastname;
+    education;
+    constructor(firstname, lastname, education) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.education = education;
+    }
+    toTableRow() {
+        return `<tr>
+        <td>${this.firstname}</td>
+        <td>${this.lastname}</td>
+        <td>${this.education}</td>
+        </tr>`;
+    }
+}
+export const persons = [
+    new Person("John", "Doe", "podstawowe"),
+    new Person("Jane", "Smith", "średnie"),
+    new Person("Mike", "Johnson", "wyższe")
+];
--- web/cw8/dist/cw8.js
+++ web/cw8/dist/cw8.js
@@ -1,16 +1,40 @@
-import { choices } from "./dane.js";
-const choice = document.querySelector("#choice");
+import { choices } from './dane.js';
+import { Person, persons } from './Person.js';
+const choice = document.querySelector('#choice');
 addOptions(choices, choice);
 // document.querySelector("#submit")?.addEventListener("click",()=>{
 //     console.log("fffffff");
 // })
-document.querySelector("#form_submit")?.addEventListener("submit", (e) => {
+document.querySelector('#form_submit')?.addEventListener('submit', (e) => {
     e.preventDefault();
-    return false;
+    const firstname = document.querySelector('#firstname')?.value;
+    const lastname = document.querySelector('#lastname')?.value;
+    const education = document.querySelector('#choice')?.value;
+    const person = new Person(firstname, lastname, education);
+    persons.push(person);
+    console.log(persons);
 });
+document.querySelector("#result").innerHTML = UpdateTable(persons);
+function UpdateTable(persons) {
+    let html = `<table class="table table-stripped">
+    <tr>
+    <th>Lp</th>  <th>Imię</th> <th>Nazwisko</th> <th>Wykształacenie</th>
+    </tr> `;
+    let lp = 0;
+    for (const p of persons) {
+        lp++;
+        html += `
+        <tr>
+        <td>${lp}</td><td>${p.firstname}</td><td>${p.lastname}</td>
+        <td>${p.education}</td>
+        </tr>
+        `;
+    }
+    return html + '</table>';
+}
 function addOptions(choices, choice) {
     for (const c of choices) {
-        const op = document.createElement("option");
+        const op = document.createElement('option');
         const txt = document.createTextNode(c);
         op.appendChild(txt);
         choice?.appendChild(op);
