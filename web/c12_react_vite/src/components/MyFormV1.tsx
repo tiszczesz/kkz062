be7ac2275be1ef18d3672075d06b4133e0f01a9a
pszczesz <pszczesz@gmail.com>
2023-12-10 16:01:03 +0100

    gg

---
+++ web/c12_react_vite/src/components/MyFormV1.tsx
@@ -0,0 +1,16 @@
+
+
+const MyFormV1 = () => {
+  return (
+    <div className="container">
+      <h1>Dane osobowe</h1>
+      <form>
+        <div className="row">
+          <label htmlFor=""></label>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default MyFormV1
--- web/cw11_react/src/App.tsx
+++ web/cw11_react/src/App.tsx
@@ -2,9 +2,11 @@ import './App.css';
 import Counter from './components/Counter';
 import Select from './components/Select';
 import SelectLight from './components/SelectLight';
+import { Dane } from './dane';
 
 function App() {
   console.log("renderowanie App");
+  console.log(`${Dane.get("a")}|${Dane.get("b")}|${Dane.get("c")}`);
   
   return (
     <div >
