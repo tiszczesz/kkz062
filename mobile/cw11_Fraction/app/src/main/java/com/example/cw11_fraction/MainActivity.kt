e937b6b924c9b74877b32bdb447e3d968f0c13e4
pszczesz <pszczesz@gmail.com>
2023-12-17 12:04:00 +0100

    ggg

--- mobile/cw11_Fraction/app/src/main/java/com/example/cw11_fraction/MainActivity.kt
+++ mobile/cw11_Fraction/app/src/main/java/com/example/cw11_fraction/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.cw11_fraction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val actions = arrayListOf<Char>('+', '-', '*', '/');
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            // R.layout.spinner_item,
+            actions
+        )
+        val actionSpiner = findViewById<Spinner>(R.id.spChoice);
+        actionSpiner.adapter = adapter;
+
+        val runButton = findViewById<Button>(R.id.btnCount);
+        val numA = findViewById<EditText>(R.id.etNumA)
+        val numB = findViewById<EditText>(R.id.etNumB)
+        val denumA = findViewById<EditText>(R.id.etDenumA)
+        val denumB = findViewById<EditText>(R.id.etDenumB)
+        val resultNum = findViewById<TextView>(R.id.tvNumResult)
+        val spinner = findViewById<Spinner>(R.id.spChoice)
+        val resultDenum = findViewById<TextView>(R.id.tvDenumResult)
+        runButton.setOnClickListener {
+            try {
+                val numberNumA: Int = numA.text.trim().toString().toInt();
+                val numberNumB: Int = numB.text.trim().toString().toInt();
+                val numberDenumA: Int = denumA.text.trim().toString().toInt();
+                val numberDenumB: Int = denumB.text.trim().toString().toInt();
+                val choicer = spinner.selectedItem as Char
+                val fracA = Fraction(numberNumA, numberDenumA)
+                val fracB = Fraction(numberNumB, numberDenumB)
+                var fracResult: Fraction
+                when (choicer) {
+                    '+' -> fracResult = fracA.AddFractions(fracB)
+                    '*' -> fracResult = fracA.MultiFractions(fracB)
+                    else -> fracResult = Fraction(1, 1)
+                }
+                resultNum.text = fracResult.numerator.toString()
+                resultDenum.text = fracResult.denumerator.toString()
+            } catch (ex: Exception) {
+                Toast.makeText(
+                    this@MainActivity, ex.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
--- mobile/cw11_Fraction/app/src/test/java/com/example/cw11_fraction/ExampleUnitTest.kt
+++ mobile/cw11_Fraction/app/src/test/java/com/example/cw11_fraction/ExampleUnitTest.kt
@@ -4,14 +4,105 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun non_zero_denumerator(){
+        val frac = Fraction(12,0);
+        assertTrue(frac.denumerator!=0)
+    }
+    @Test
+    fun denumerator_one(){
+        val frac = Fraction(12,0);
+        assertTrue(frac.denumerator==1)
+    }
+    @Test
+    fun denumerator_other(){
+        val frac = Fraction(12,7);
+        assertTrue(frac.denumerator==7)
+    }
+    @Test
+    fun getNWD(){
+        val frac = Fraction(-12,-8);
+        assertEquals(4,frac.getNwd(12,8))
+    }
+    @Test
+    fun getNWD2(){
+        val frac = Fraction(6,18);
+        assertEquals(6,frac.getNwd(6,18))
+    }
+    @Test
+    fun getNWD3(){
+        val frac = Fraction(12,7);
+        assertEquals(1,frac.getNwd(12,7))
+    }
+
+    @Test
+    fun getCommonDenumerator(){
+        val frac1 = Fraction(4,3)
+        val frac2 = Fraction(1,2)
+        assertEquals(6,frac1.getCommon(frac2))
+    }
+
+    @Test
+    fun useSimplifier1(){
+        val frac1 = Fraction(2,4)
+        val actual = frac1.simplifier();
+        assertEquals(1,actual.numerator);
+        assertEquals(2,actual.denumerator);
+    }
+    @Test
+    fun useSimplifier2(){
+        val frac1 = Fraction(3,4)
+        val actual = frac1.simplifier();
+        assertEquals(3,actual.numerator);
+        assertEquals(4,actual.denumerator);
+    }
+
+    @Test
+    fun multiFrac1(){
+        val frac1 = Fraction(3,4)
+        val frac2 = Fraction(1,2)
+        val actual = frac1.MultiFractions(frac2);
+        assertEquals(3,actual.numerator)
+        assertEquals(8,actual.denumerator)
+    }
+    @Test
+    fun multiFrac2(){
+        val frac1 = Fraction(2,4)
+        val frac2 = Fraction(1,2)
+        val actual = frac1.MultiFractions(frac2);
+        assertEquals(1,actual.numerator)
+        assertEquals(4,actual.denumerator)
+    }
+
+    @Test
+    fun addFrac1(){
+        val frac1 = Fraction(2,4)
+        val frac2 = Fraction(1,2)
+        val actual = frac1.AddFractions(frac2);
+        assertEquals(1,actual.numerator)
+        assertEquals(1,actual.denumerator)
+    }
+    @Test
+    fun addFrac2(){
+        val frac1 = Fraction(1,3)
+        val frac2 = Fraction(3,4)
+        val actual = frac1.AddFractions(frac2);
+        assertEquals(13,actual.numerator)
+        assertEquals(12,actual.denumerator)
+    }
+    @Test
+    fun Subtraction(){
+        val frac1 = Fraction(1,3)
+        val frac2 = Fraction(1,2)
+        val actual = frac1.SubtractionFractions(frac2);
+        assertEquals(-1,actual.numerator)
+        assertEquals(6,actual.denumerator)
     }
 }
