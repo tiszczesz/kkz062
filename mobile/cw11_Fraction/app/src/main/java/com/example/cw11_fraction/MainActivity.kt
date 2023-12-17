38e3f26de4ae90a3ed2997debcabd7c4afaf8515
pszczesz <pszczesz@gmail.com>
2023-12-17 09:29:14 +0100

    hh

---
+++ mobile/cw11_Fraction/app/src/main/java/com/example/cw11_fraction/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cw11_fraction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
