2819b754d95b4cba3cd1abf251450bae10c340ea
pszczesz <pszczesz@gmail.com>
2024-01-21 11:16:20 +0100

    ff

---
+++ web/cw19-book-list/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Hello from react-bootstrap</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
