da1adbe1083d78404084c5c628135545d173d995
pszczesz <pszczesz@gmail.com>
2024-02-23 19:28:38 +0100

    gg

--- mobile/jeComp_cw2/app/src/main/java/com/example/jecomp_cw2/MainActivity.kt
+++ mobile/jeComp_cw2/app/src/main/java/com/example/jecomp_cw2/MainActivity.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.magnifier
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,13 +50,24 @@ fun All() {
         }
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.height(50.dp).border(
-                width = 1.dp,color = Color.Magenta
-            )
+            modifier = Modifier
+                .height(50.dp)
+                .border(
+                    width = 1.dp, color = Color.Magenta
+                )
         ) {
             Text(text = "dddd", modifier = Modifier.weight(1f))
 
             Text(text = "ala ma kota", modifier = Modifier.weight(1f))
+            Text(text = "ala ma kota", modifier = Modifier.weight(1f))
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.height(30.dp).align(alignment = Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Bla bla bla", modifier = Modifier.width(100.dp))
+            Text(text = "Bla bla bla")
         }
     }
 
