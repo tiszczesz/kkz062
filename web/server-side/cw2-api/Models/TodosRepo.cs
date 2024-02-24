2be45c244955037dd1a3bfc38d94b4c7f098d2b8
pszczesz <pszczesz@gmail.com>
2024-02-24 15:39:13 +0100

    ff

--- web/server-side/cw2-api/Models/TodosRepo.cs
+++ web/server-side/cw2-api/Models/TodosRepo.cs
@@ -1,12 +1,48 @@
-﻿namespace cw2_api;
+﻿using MySql.Data.MySqlClient;
+
+namespace cw2_api;
 
 public class TodosRepo
 {
     private string _connString;
-    public TodosRepo(string connString){
+    public TodosRepo(string connString)
+    {
         _connString = connString;
     }
-    public List<Todo> GetAll(){
-        
+    public List<Todo> GetAll()
+    {
+        List<Todo> todos = new List<Todo>();
+        MySqlConnection connection = new MySqlConnection(_connString);
+        string sql = "SELECT id,name,description,isComplete FROM todos";
+        MySqlCommand command = connection.CreateCommand();
+        command.CommandText = sql;
+        connection.Open();
+        MySqlDataReader rd = command.ExecuteReader();
+        if (rd.HasRows)
+        {
+            while (rd.Read())
+            {
+                todos.Add(new Todo
+                {
+                    Id = rd.GetInt32(0),
+                    Name = rd.GetString(1),
+                    Description = rd.GetString(2),
+                    IsComplete = rd.GetBoolean(3)
+                });
+            }
+        }
+        connection.Close();
+        return todos;
+    }
+    public void InsertTodo(Todo todo)
+    {
+        MySqlConnection connection = new MySqlConnection(_connString);
+        MySqlCommand command = connection.CreateCommand();
+        command.CommandText =
+               $"INSERT INTO todos(name,description,isComplete)" +
+        $" VALUES('{todo.Name}','{todo.Description}',{todo.IsComplete})";
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 }
--- web/server-side/cw2-api/Program.cs
+++ web/server-side/cw2-api/Program.cs
@@ -1,9 +1,13 @@
+using cw2_api;
+
 var builder = WebApplication.CreateBuilder(args);
 string? connString = builder.Configuration
    .GetConnectionString("mysqlConn");
-
+TodosRepo repo = new TodosRepo(connString);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/api/todos",()=>repo.GetAll());
+app.MapPost("/api/todos",(Todo todo)=>repo.InsertTodo(todo));
+app.MapDelete("/api/todos",(int? id)=>repo.DeleteTodo(id));
 app.Run();
